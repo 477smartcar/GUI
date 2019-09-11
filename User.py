6b62ea168c740f189ba11ab0e86556270c35cd78
tsamying <tsamying0626@gmail.com>
2019-09-11 17:19:46 -0400

    updated

--- User.py
+++ User.py
@@ -3,11 +3,23 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 from user_input import *
 
 
-class User(QMainWindow, Ui_MainWindow):
+class User(QMainWindow, Ui_mainWindow):
+    dstNum = -1
 
     def __init__(self, parent=None):
         super(User, self).__init__(parent)
         self.setupUi(self)
+
+        self.btnOK.setDisabled(True)
+        self.txtEnter.textChanged.connect(self.checkEnter)
+
+        self.btnOK.clicked.connect(self.saveDst)
+
+    def checkEnter(self):
+        self.btnOK.setEnabled(True) if self.txtEnter.text() != "" else self.btnOK.setDisabled(True)
+
+    def saveDst(self):
+        User.dstNum = int(self.txtEnter.text())
 
 
 if __name__ == "__main__":
